10cf03c3731e389cbf6165a0f9e17733f16c4add
dmitry.kalyuzhnyi <dmitry.kalyuzhnyi@gmail.com>
2015-12-21 19:42:41 +0300

    ADDED user actions handling

---
+++ app/src/main/java/com/dka/profin/CategoryItemClickListener.java
@@ -0,0 +1,8 @@
+package com.dka.profin;
+
+/**
+ * @author Dmitry.Kalyuzhnyi 21.12.2015.
+ */
+public interface CategoryItemClickListener {
+    void onItemClick(int position);
+}
--- app/src/main/java/com/dka/profin/fragment/RootFragment.java
+++ app/src/main/java/com/dka/profin/fragment/RootFragment.java
@@ -1,8 +1,12 @@
 package com.dka.profin.fragment;
 
+import android.content.AsyncQueryHandler;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
@@ -10,13 +14,13 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.dka.mainmenu.ContentFragment;
+import com.dka.profin.CategoryItemClickListener;
 import com.dka.profin.R;
 import com.dka.profin.common.CursorRecyclerViewAdapter;
 import com.dka.profin.data.contract.CategoryContract;
@@ -25,10 +29,14 @@ import com.dka.profin.data.contract.MainContract;
 /**
  * @author Dmitry.Kalyuzhnyi 14.12.2015.
  */
-public class RootFragment extends ContentFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class RootFragment extends ContentFragment implements LoaderManager.LoaderCallbacks<Cursor>,
+        CategoryItemClickListener {
+    private final static int ROOT_FRAGMENT_LOADER = 0;
+
     private RecyclerView mRecyclerView;
     private CategoryAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private QueryHandler mQueryhandler;
 
     public RootFragment() {
     }
@@ -49,7 +57,10 @@ public class RootFragment extends ContentFragment implements LoaderManager.Loade
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        getLoaderManager().initLoader(0, null, this);
+        getLoaderManager().initLoader(ROOT_FRAGMENT_LOADER, null, this);
+        if(mQueryhandler == null){
+            mQueryhandler = new QueryHandler(getContext().getContentResolver());
+        }
     }
 
 
@@ -61,7 +72,6 @@ public class RootFragment extends ContentFragment implements LoaderManager.Loade
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        Log.d(">>>", DatabaseUtils.dumpCursorToString(data));
         if (data != null && mAdapter.getCursor() != data) {
             mAdapter.swapCursor(data);
         }
@@ -72,17 +82,49 @@ public class RootFragment extends ContentFragment implements LoaderManager.Loade
         mAdapter.swapCursor(null);
     }
 
-    private static class CategoryViewHolder extends RecyclerView.ViewHolder {
+    private static class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final CategoryItemClickListener mCategoryItemClickListener;
+
         final TextView mName;
         final TextView mProportion;
         final TextView mSum;
 
-        public CategoryViewHolder(View itemView) {
+        public CategoryViewHolder(View itemView,
+                                  CategoryItemClickListener listener) {
             super(itemView);
+
+            mCategoryItemClickListener = listener;
+            itemView.setOnClickListener(this);
 
             mName = (TextView) itemView.findViewById(R.id.tv_cat_name);
             mProportion = (TextView) itemView.findViewById(R.id.tv_cat_proportion);
             mSum = (TextView) itemView.findViewById(R.id.tv_cat_sum);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mCategoryItemClickListener != null) {
+                mCategoryItemClickListener.onItemClick(getAdapterPosition());
+            }
+        }
+    }
+
+    /**
+     * Implementation for {@link CategoryItemClickListener}
+     *
+     * @param position
+     */
+    @Override
+    public void onItemClick(int position) {
+        final Cursor cursor = mAdapter.getCursor();
+        cursor.moveToPosition(position);
+        int id = cursor.getInt(cursor.getColumnIndex(CategoryContract.Columns._ID));
+        if (mQueryhandler != null) {
+            final ContentValues cv = new ContentValues();
+            cv.put(MainContract.Columns.CATEGORY_ID, id);
+            cv.put(MainContract.Columns.SUM, 10);
+            cv.put(MainContract.Columns.DATE_TIME, System.currentTimeMillis());
+            mQueryhandler.startInsert(0, null, MainContract.CONTENT_URI, cv);
         }
     }
 
@@ -96,12 +138,25 @@ public class RootFragment extends ContentFragment implements LoaderManager.Loade
             viewHolder.mName.setText(cursor.getString(cursor.getColumnIndex(CategoryContract.Columns.NAME)));
             viewHolder.mProportion.setText(cursor.getString(cursor.getColumnIndex(CategoryContract.Columns.PROPORTION)));
             viewHolder.mSum.setText(cursor.getString(cursor.getColumnIndex(MainContract.Columns.SUM)));
+
         }
 
         @Override
         public CategoryViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
-            return new CategoryViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_category, parent, false));
+            return new CategoryViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_category, parent, false),
+                    RootFragment.this);
+        }
+    }
+
+    public class QueryHandler extends AsyncQueryHandler {
+        public QueryHandler(ContentResolver cr) {
+            super(cr);
+        }
+
+        @Override
+        protected void onInsertComplete(int token, Object cookie, Uri uri) {
+            getLoaderManager().getLoader(ROOT_FRAGMENT_LOADER).forceLoad();
         }
     }
 }
