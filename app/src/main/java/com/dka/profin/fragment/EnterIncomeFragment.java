fc72d0ed968298efe5d5b428a9fa517a0cca1bb4
dmitry.kalyuzhnyi <dmitry.kalyuzhnyi@gmail.com>
2015-12-30 19:05:52 +0300

    ADDED saving record to income fragment

--- app/src/main/java/com/dka/profin/fragment/EnterIncomeFragment.java
+++ app/src/main/java/com/dka/profin/fragment/EnterIncomeFragment.java
@@ -1,5 +1,10 @@
 package com.dka.profin.fragment;
 
+import android.content.ContentValues;
+
+import com.dka.profin.data.contract.IncomeContract;
+import com.dka.profin.data.contract.MainContract;
+
 /**
  * @author Dmitry.Kalyuzhnyi 30.12.2015.
  */
@@ -8,6 +13,21 @@ public class EnterIncomeFragment extends EnterDataFragment {
 
     @Override
     protected boolean addRecord() {
-        return false;
+        final String sumStr = vSum.getText().toString();
+        if (!sumStr.isEmpty()) {
+            if (mQueryhandler == null) {
+                mQueryhandler = new QueryHandler(getContext().getContentResolver());
+            }
+
+            final ContentValues cv = new ContentValues();
+            cv.put(IncomeContract.Columns.CATEGORY_ID, mCateroryId);
+            cv.put(IncomeContract.Columns.SUM, Integer.parseInt(sumStr));
+            cv.put(IncomeContract.Columns.DATE_TIME, System.currentTimeMillis());
+            mQueryhandler.startInsert(0, null, IncomeContract.CONTENT_URI, cv);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
